20bcb32e136ff3c1ae89639417c7e8d1f1f71d69
Barracuda72 <barracuda72@bk.ru>
2013-11-13 16:27:31 +0600

    Сортировка бинарными вставками

---
+++ Praktika8/Sort_binary.pas
@@ -0,0 +1,25 @@
+procedure Sort(n : integer; var a : TArray);
+var 
+  i,j,l,r,m : integer;
+  x : Item;
+begin
+  for i := 2 to n do
+    if A[i-1].key > A[i].key then begin 
+      x := A[i];
+      l := 1;
+      r := i-1;
+      repeat
+        m := (l+r) div 2; {Новой "серединой" 
+                      последовательности всегда будет 
+                           левый центральный элемент}
+        if A[m].key < x.key 
+         then 
+           l := m+1
+         else 
+           r := m-1;
+      until l > r; {поиск ведется до тех пор, пока левая граница не окажется правее(!) правой границы}
+      for j := i-1 downto l do 
+        A[j+1] := A[j];
+      A[l] := x;
+    end;
+end;
