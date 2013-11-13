0c50df4e565100e9b1364deea3deca8e54082dcb
Barracuda72 <barracuda72@bk.ru>
2013-11-13 16:27:44 +0600

    Сортировка Шелла

---
+++ Praktika8/Sort_shell.pas
@@ -0,0 +1,36 @@
+const
+  t = 4;
+  h : array [1..t] of integer = (11, 7, 4, 1);
+
+procedure Sort(n : integer; var A : TArray);
+var
+   i,j,k,m : integer;
+   h : array [1..t] of byte;
+   x : people;
+begin
+{ Сортировка методом Шелла }
+
+  for m := 1 to t do begin
+    k := h[m];
+
+    i := 1;
+
+    { Вспомогательная сортировка пузырьком }
+    while (i <= n - k) do begin { Пробегаем массив }
+      j := i + k;
+
+      while (j <= n) do begin
+        if (p[i] < p[j]) then begin
+          { Меняем элементы местами }
+          x := p[i];
+          p[i] := p[j];
+          p[j] := x;
+        end;
+        j := j + k;
+      end;
+
+      i := i + 1;
+    end;
+
+  end;
+end;
